d787c98d80e58a564bdc1783a53c8aeb5c714ec8
Plamen Dobrev <plambata95@abv.bg>
2020-09-11 18:15:02 +0300

    Add project files.

---
+++ Plamen-Dobrev-Employees/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Plamen_Dobrev_Employees.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
